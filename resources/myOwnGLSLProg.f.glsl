27ce1a8dfc56d426f4173e6ef347967275079e31
loic <loic.simon@ensicaen.fr>
2015-09-16 20:06:24 +0200

    Added fake shader for from scratch.

---
+++ resources/myOwnGLSLProg.f.glsl
@@ -0,0 +1,7 @@
+#version 410
+/*!todo: Create a fragment shader
+  * Lab0: simple version
+  * Lab1: textured version
+  * Lab2: fancier version (shading, cartoon, ...)
+  */
+
---
+++ resources/myOwnGLSLProg.v.glsl
@@ -0,0 +1,6 @@
+#version 410
+/*!todo: Create a vertex shader
+  * Lab0: simple version
+  * Lab1: textured version
+  * Lab2: fancier version (shading, cartoon, ...)
+  */
