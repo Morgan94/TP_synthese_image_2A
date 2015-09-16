20b7d3eafecac596f9d02613c207a33ce88d906a
loic <loic.simon@ensicaen.fr>
2015-09-16 10:28:23 +0200

    Included the lab0.

---
+++ fromScratch/myOwnOpenGLProg.cpp
@@ -0,0 +1,20 @@
+/* Include headers*//*{{{*/
+#include <iostream>
+#include "myOwnOpenGLProg.h"/*}}}*/
+
+/*!todo: You must code an openGL application from scratch here;
+ * You can rely on openGL, glfw3, glm BUT NOT ON the labs api: lightGLAPI.h
+ * Here are the details of your job:
+ * Lab 0: Create a triangle
+ * Lab 1: Create some more complex primitive (e.g parametric) and handle
+ * occlusions
+ * Lab 2: Add some textures
+ * Lab 3: Add some nice shading effects
+ * 
+ * */
+int main(void)/*{{{*/
+{
+    std::cout << "Yes we can!!!" << std::endl;
+}/*}}}*/
+
+// vim: ft=cpp foldmethod=marker:
